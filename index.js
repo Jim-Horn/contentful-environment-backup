f44b150359338f17d47ca119e37343569b8f7801
Jim Horn <jim@jdhorn.com>
2022-12-23 12:09:58 -0800

    Remove functions from `index.js`

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
 #!/usr/bin/env node
-const { execSync } = require('child_process');
+import { execSync } from 'child_process';
+import getAndParseContentfulEnvironments from './utils/getAndParseContentfulEnvironments.js';
+import getBackupName from './utils/getBackupName.js';
+
+const backupNameExt = 'BAK';
 
 function executeCommand(cmd) {
     return execSync(cmd).toString();
 }
 
-function getBackupName() {
-    const currentDate = new Date();
-    let returnString = 'BACKUP-';
-    returnString += currentDate.getFullYear();
-    returnString += pad(currentDate.getMonth() + 1);
-    returnString += pad(currentDate.getDate());
-    returnString += '-';
-    returnString += pad(currentDate.getHours());
-    returnString += pad(currentDate.getMinutes());
+getAndParseContentfulEnvironments();
+// console.log('JSON.stringify(process.env, null, 2): ', JSON.stringify(process.env, null, 2));
 
-    return returnString;
-
-    function pad(val) {
-        return val.toString().padStart(2, '0');
-    }
-}
-
-const managementToken = process.env['INPUT_CONTENTFUL-CONTENT-MANAGEMENT-TOKEN'];
-const spaceId = process.env['INPUT_CONTENTFUL-SPACE-ID'];
+// const managementToken = process.env['INPUT_CONTENTFUL-CONTENT-MANAGEMENT-TOKEN'];
+const managementToken = "process.env['INPUT_CONTENTFUL-CONTENT-MANAGEMENT-TOKEN']";
+// const spaceId = process.env['INPUT_CONTENTFUL-SPACE-ID'];
+const spaceId = "process.env['INPUT_CONTENTFUL-SPACE-ID']";
 const accessString = `--management-token ${managementToken} --space-id ${spaceId}`;
 
 try {
     const deleteOldBackupCommand = `contentful space environment delete --environment-id AUTOMATIC-BACKUP ${accessString}`;
     console.log(`Delete command: ${deleteOldBackupCommand}`);
-    executeCommand(deleteOldBackupCommand);
+    // executeCommand(deleteOldBackupCommand);
 }catch (err){
     console.log(err)
 }
 
 try {
-    const createBackupCommand = `contentful space environment create --name ${getBackupName()} --environment-id AUTOMATIC-BACKUP ${accessString}`;
+    const createBackupCommand = `contentful space environment create --name ${getBackupName(backupNameExt)} --environment-id AUTOMATIC-BACKUP ${accessString}`;
     console.log(`Backup command: ${createBackupCommand}`);
-    executeCommand(createBackupCommand);
+    // executeCommand(createBackupCommand);
 }catch (err){
     console.log(err)
 }
