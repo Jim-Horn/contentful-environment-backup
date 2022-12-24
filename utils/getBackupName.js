79af20bf4f71e6c40168430903298236f7c9b0c4
Jim Horn <jim@jdhorn.com>
2022-12-24 07:07:11 -0800

    Update date-time separator

--- utils/getBackupName.js
+++ utils/getBackupName.js
@@ -2,7 +2,7 @@
 import getDateString from "./getDateString.js";
 
 function getBackupName(backupNameExt) {
-  return `${getDateString(new Date())}-${backupNameExt}`;
+    return `${getDateString(new Date())}_${backupNameExt}`;
 }
 
 export default getBackupName;
