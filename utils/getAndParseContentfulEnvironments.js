f86dc84875fe0bda1e55bd74b0c2f3a40371939d
Jim Horn <jim@jdhorn.com>
2022-12-24 07:08:19 -0800

    Refactor `getAndParseContentfulEnvironments`

--- utils/getAndParseContentfulEnvironments.js
+++ utils/getAndParseContentfulEnvironments.js
@@ -1,36 +1,64 @@
 #!/usr/bin/env node
-import fs from 'fs';
-import { execSync } from 'child_process';
+import fs from "fs";
+import { execSync } from "child_process";
 
-const DATA_DIR = 'data';
+const DATA_DIR = "data";
 
 function getAndParseContentfulEnvironments() {
-    executeCommand(`contentful space environment list > ${DATA_DIR}/environments.txt`);
+    getContentfulEnvironments();
 
-    const results = [];
-    let data = fs.readFileSync(`${DATA_DIR}/environments.txt`, { encoding: 'utf8', flag: 'r' });
-    data = data.replace(/(┌|┐|├|┼|┤|└|┴|┘|┬|─|│)/g, '');
-    data = data.replace(/( ){2,}/g, ',');
-    data = data.replace(/^( )/g, '');
-    data = data.replace(/\x1B\[90m\x1B\[39m/g, '');
-    let dataArr = data.split('\n');
-    dataArr = dataArr.filter(el => el !== '');
-    dataArr.shift();
-    dataArr.forEach(el => results.push(makeObj(el)));
+    const results = createEnvironmentsObj();
 
-    fs.writeFile(`${DATA_DIR}/envs.json`, JSON.stringify(results), err => {
-        if (err) throw err;
-        console.log('Data written to file');
-    });
+    writeJsonFile(results);
+
+    function getContentfulEnvironments() {
+        executeCommand(
+            `contentful space environment list > ${DATA_DIR}/environments.txt`
+        );
+    }
+
+    function createEnvironmentsObj() {
+        const results = [];
+        let dataArr = cleanData(
+            fs.readFileSync(`${DATA_DIR}/environments.txt`, {
+                encoding: "utf8",
+                flag: "r",
+            })
+        ).split("\n");
+        dataArr = dataArr.filter((el) => el !== "");
+        dataArr.shift();
+        dataArr.forEach((el) => results.push(makeObj(el)));
+        return results;
+
+        function cleanData(data) {
+            let returnVal = data.replace(/(┌|┐|├|┼|┤|└|┴|┘|┬|─|│)/g, "");
+            returnVal = returnVal.replace(/( ){2,}/g, ",");
+            returnVal = returnVal.replace(/^( )/g, "");
+            returnVal = returnVal.replace(/\x1B\[90m\x1B\[39m/g, "");
+            return returnVal;
+        }
+
+        function makeObj(el) {
+            const elArr = el.replace(/( ){2,}/g, ",").split(",");
+            const [name, id, status] = elArr;
+            return { name: name.trim(), id: id.trim(), status: status.trim() };
+        }
+    }
+
+    function writeJsonFile(results) {
+        fs.writeFile(
+            `${DATA_DIR}/envs.json`,
+            JSON.stringify(results, null, 4),
+            (err) => {
+                if (err) throw err;
+                console.log("Data written to file");
+            }
+        );
+    }
 
     function executeCommand(cmd) {
         return execSync(cmd).toString();
     }
-
-    function makeObj(el) {
-        const elArr = el.replace(/( ){2,}/g, ',').split(',');
-        const [name, id, status] = elArr;
-        return { name: name.trim(), id: id.trim(), status: status.trim() };
-    }
 }
+
 export default getAndParseContentfulEnvironments;
