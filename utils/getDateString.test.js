81d0282405b7cf3697b8e7b6d8d98a33fd743a61
Jim Horn <jim@jdhorn.com>
2022-12-27 13:14:19 -0800

    Add basic tests

---
+++ utils/getDateString.test.js
@@ -0,0 +1,11 @@
+import getDateString from "./getDateString";
+
+test('getDateString should properly parse the date', ()=>{expect(
+                                                              getDateString(
+                                                                  new Date(
+                                                                      "2022-12-27T20:46:18.736Z"
+                                                                  )
+                                                              )
+                                                          ).toBe(
+                                                              "2022-12-27_12-46-18"
+                                                          );})
